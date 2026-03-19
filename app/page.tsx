18ae22f5da7e7f2ea5d1dc7f2b0ecc64b83b913e
Vegard Westermoen <vegard.westermoen@gmail.com>
2026-03-19 17:51:22 +0100

    har startet

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1>hei jeg er vegard</h1>
+    <div className="text-center">
+      <section>
+        <h1 className="text-white text-4xl font-bold mt-10">
+          Hei, jeg er Vegard, velkommen til min hjemmeside!
+        </h1>
+        <p className=" text-white text-2xl font-semibold mt-5">
+          Her kan du se porteføljen min, personalia og mye annet.
+        </p>
+      </section>
+
+      <section className="mt-8">
+        <h2 className="text-2xl text-white">Mine prosjekter</h2>
+      </section>
+
     </div>
   );
 }
