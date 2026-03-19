f180647b25d579d946fbc9f648d282fb283ade6f
Vegard Westermoen <vegard.westermoen@gmail.com>
2026-03-19 19:22:51 +0100

    Laget flere filer

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
   return (
-    <div className="text-center">
-      <section>
-        <h1 className="text-white text-4xl font-bold mt-10">
-          Hei, jeg er Vegard, velkommen til min hjemmeside!
-        </h1>
-        <p className=" text-white text-2xl font-semibold mt-5">
-          Her kan du se porteføljen min, personalia og mye annet.
-        </p>
-      </section>
-
-      <section className="mt-8">
-        <h2 className="text-2xl text-white">Mine prosjekter</h2>
-      </section>
-
-    </div>
+    <main className="min-h-screen flex items-center justify-center text-white">
+      <Hero />
+    </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+      <section className = "text-center text-bold text-4xl font-semibold">
+        VEGARD WESTERMOEN
+        <h1 className = "font-bold text-4xl">HJEMMESIDE</h1>
+        <p className = "font-bold text-3xl text-center"> Hei, velkommen, dette er min hjemmeside!</p>
+      </section>
+    );
+  }
